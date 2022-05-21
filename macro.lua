82f5eb4c1b684da691c01feed8bb89bb4bd2acb0
bubatz <71530056+cynhook@users.noreply.github.com>
2022-05-21 18:34:02 -0400

    Update macro.lua

--- macro.lua
+++ macro.lua
@@ -1,1 +1,26 @@
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
+--Values to Edit
+local defaultSpeed = 16 --This is the default roblox character speed dont change unless you have to
+local speed = 50 -- This is the speed you want to change to after E is pressed
+
+-- Dont edit anything below this line unless you know what you're doing
+
+_G.Speeding = true
+
+local isSpeeding = false
+
+game:GetService("UserInputService").InputBegan:connect(function(inputkey)
+   if inputkey.KeyCode == Enum.KeyCode.C then
+       if isSpeeding == false then
+           
+           game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speed
+           isSpeeding = true
+           else
+               
+               game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = defaultSpeed
+               isSpeeding = false
+           end
+       
+       
+       end
+   
+   end)
