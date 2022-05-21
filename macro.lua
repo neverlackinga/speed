b7ab679ab8f18b60a4437c386e98c7e75a9e2040
bubatz <71530056+cynhook@users.noreply.github.com>
2022-05-21 18:35:30 -0400

    Update macro.lua

--- macro.lua
+++ macro.lua
@@ -1,26 +1,25 @@
---Values to Edit
-local defaultSpeed = 16 --This is the default roblox character speed dont change unless you have to
-local speed = 50 -- This is the speed you want to change to after E is pressed
+--Settings
+local sped = 110 -- Speed
+local keybind = "c"
 
--- Dont edit anything below this line unless you know what you're doing
 
-_G.Speeding = true
 
-local isSpeeding = false
 
-game:GetService("UserInputService").InputBegan:connect(function(inputkey)
-   if inputkey.KeyCode == Enum.KeyCode.C then
-       if isSpeeding == false then
-           
-           game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speed
-           isSpeeding = true
-           else
-               
-               game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = defaultSpeed
-               isSpeeding = false
-           end
-       
-       
-       end
-   
-   end)
+--The Script
+
+yes = false
+	plr = game.Players.LocalPlayer
+	mouse = plr:GetMouse()
+	mouse.KeyDown:connect(function(key)
+		if key == keybind and yes == false then
+			yes = true
+			game.Players.LocalPlayer.Character.Humanoid.Name = "Humz"
+			game.Players.LocalPlayer.Character.Humz.WalkSpeed = sped
+			game.Players.LocalPlayer.Character.Humz.JumpPower = 50
+		elseif key == keybind and yes == true then
+			yes = false
+			game.Players.LocalPlayer.Character.Humz.WalkSpeed = 16
+			game.Players.LocalPlayer.Character.Humz.JumpPower = 50
+			game.Players.LocalPlayer.Character.Humz.Name = "Humanoid"
+		end
+	end)
